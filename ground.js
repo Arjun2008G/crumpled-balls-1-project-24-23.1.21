29db009cc1ec95acf9fbf0c40f16581bd60ca592
Arjun2008G <75516199+Arjun2008G@users.noreply.github.com>
2021-01-23 17:19:07 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,26 @@
+class ground {
+constructor(x,y,width,height){
+var op={
+
+    isStatic:true
+}
+this.x=x;
+this.y=y;
+this.width=width;
+this.height=height;
+
+this.body=Bodies.rectangle(x,y,width,height,op);
+
+World.add(world,this.body);
+}
+display(){
+var pos=this.body.position;
+push();
+translate(pos.x,pos.y);
+rectMode(CENTER);
+strokeWeight(4);
+fill(255,255,0);
+rect(0,0,this.width,this.height);
+pop();
+}
+};
